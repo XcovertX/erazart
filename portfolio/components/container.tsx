09aaa3c21d68cf6e7f5ea554fd0904d34e856c24
James Covert <jcovert2@asu.edu>
2023-09-22 09:52:12 -0700

    added analytics

--- portfolio/components/container.tsx
+++ portfolio/components/container.tsx
@@ -1,10 +1,15 @@
+import { Analytics } from '@vercel/analytics/react';
+
 type Props = {
   children?: React.ReactNode
 }
 
 const Container = ({ children }: Props) => {
   return (
-  <div className="container mx-auto px-5">{children}</div>
+  <div className="container mx-auto px-5">
+    {children}
+    <Analytics />
+    </div>
   )
 }
 
